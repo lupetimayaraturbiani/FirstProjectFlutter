65a7809541835b5db73316fa0b5b885b841f37c5
lupetimayaraturbiani <mayaralupetiturbiani@gmail.com>
2021-08-12 17:37:49 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Contador de pessoas",
+    home: Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpeg",
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas: 0", 
+              style: TextStyle(color: Colors.white, 
+              fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, 
+                      color: Colors.white)
+                    ),
+                    onPressed: () {}, 
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, 
+                      color: Colors.white)
+                    ),
+                    onPressed: () {}, 
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              "Pode Entrar!", 
+              style: TextStyle(color: Colors.white, 
+              fontStyle: FontStyle.italic,
+              fontSize: 30.0),
+            )
+          ],
+        )
+      ],
+    )
+  ));
+}
